61d418db45ed0de7acf368a6dc6cb381569ac2b2
定宇 任 <ren@ntrwkpd.onmicrosoft.com>
2022-10-29 14:44:18 +0800

    修改项目结构

---
+++ Options/AesOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace CksysRecruitNew.Server.Options; 
+
+public class AesOptions {
+  
+  public const string SectionKey = "AesOptions";
+  public string EncryptKey { get; set; } = string.Empty;
+}
